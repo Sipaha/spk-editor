af7b6d4858c16bdc262543257e3e7290de155f83
Pavel Simonov <sipahabk@gmail.com>
2026-04-25 07:35:11 +0700

    uninstall: Revert .zed_server path to match preserved internal identifier

--- script/uninstall.sh
+++ script/uninstall.sh
@@ -101,7 +101,7 @@ linux() {
         prompt_remove_preferences
     fi
 
-    rm -rf $HOME/.spk-editor_server
+    rm -rf $HOME/.zed_server
 }
 
 macos() {
@@ -152,7 +152,7 @@ macos() {
         prompt_remove_preferences
     fi
 
-    rm -rf $HOME/.spk-editor_server
+    rm -rf $HOME/.zed_server
 }
 
 main "$@"
